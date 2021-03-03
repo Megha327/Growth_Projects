21b230cbe285ba1e972af1beeee7f01a8fa0d998
Megha327 <megha@tothenew.com>
2021-03-03 20:25:50 +0530

    commit with creating a react_webpack to do app folder

---
+++ react_webpack/webpack.config.js
@@ -0,0 +1,40 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: './client/index.html',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.html'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
